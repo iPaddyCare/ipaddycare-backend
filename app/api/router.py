a99f9fabc31d85c7ff9a710a3d0ba6b12ca9ee5a
Sadeepa Atigala <115334847+sadeepaatigala@users.noreply.github.com>
2026-03-07 17:37:58 +0530

    added api for seed moisture prediction

--- app/api/router.py
+++ app/api/router.py
@@ -1,7 +1,12 @@
 from fastapi import APIRouter
-from app.api.v1 import health, rice_variety
+from app.api.v1 import health, rice_variety, seed_moisture
 
 api_router = APIRouter(prefix="/api/v1")
 
 api_router.include_router(health.router, tags=["Health"])
 api_router.include_router(rice_variety.router, prefix="/rice-variety", tags=["Rice Variety"])
+api_router.include_router(
+    seed_moisture.router,
+    prefix="/seed-moisture",
+    tags=["Seed Moisture"],
+)
